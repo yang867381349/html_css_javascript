ba1d1a4bf14694e2fb1e30ca6b3a155cafac3957
867381349@qq.com <867381349@qq.com>
2024-07-28 12:51:51 +0800

    添加一些实用的方法

---
+++ 判断奇数.js
@@ -0,0 +1,8 @@
+/*
+* 判断奇数
+* @param {Number} num
+* @return {Boolean}
+* */
+function isOdd:boolean(num) {
+  return num % 2 === 1 && num % 2 === -1;
+}
---
+++ 判断奇数.ts
@@ -0,0 +1,8 @@
+/*
+* 判断奇数
+* @param {Number} num
+* @return {Boolean}
+* */
+function isOdd(num:number): boolean {
+  return num % 2 === 1 && num % 2 === -1;
+}
